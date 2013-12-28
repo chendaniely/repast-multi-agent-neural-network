4d5303754bb218d7c95b31bcc221773155d01d5e
Daniel Chen <Maveri2k@yahoo.com>
2013-12-27 21:36:06 -0500

    Moved test and unused classes

---
+++ Daniel/src/unused/ExampleWriteToTxt.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package unused;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author dchen
+ *
+ */
+public class ExampleWriteToTxt {
+	
+/////////////////////////////////////
+
+/** The below Java Code writes 
+* some data to a file in CSV
+* (Comma Separated Value) File
+*/
+	public static void main(String[] args) throws IOException {
+		//Note the "\\" used in the path of the file instead of "\",
+		//this is required to read the path in the String format.
+		FileWriter fw = new FileWriter("./src/files/TestTxtOutput.txt");
+		PrintWriter pw = new PrintWriter(fw);
+
+		//Write to file line by line
+		pw.println("Hello guys");
+		pw.println("Java Code Online is testing");
+		pw.println("writing to a file operation");
+
+		//Flush the output to the file
+		pw.flush();
+
+		//Close the Print Writer
+		pw.close();
+
+		//Close the File Writer
+		fw.close(); 
+		
+		// used to find out where the reference directory is!
+		System.out.println(System.getProperty("user.dir"));
+		}
+}
