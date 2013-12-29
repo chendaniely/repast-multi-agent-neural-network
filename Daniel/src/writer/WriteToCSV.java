d533e97301669c6fec3bdf999ba090975e92fc23
Daniel Chen <Maveri2k@yahoo.com>
2013-12-28 20:18:33 -0500

    Method to write rows to CSV

---
+++ Daniel/src/writer/WriteToCSV.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package writer;
+
+/**
+ * @author dchen
+ *
+ */
+public class WriteToCSV {
+
+	/**
+	 * 
+	 */
+	public WriteToCSV(String fileDirectory, String[] arrayOfValues) {
+		System.out.println("Writing to CSV file");
+		
+	}
+
+}
