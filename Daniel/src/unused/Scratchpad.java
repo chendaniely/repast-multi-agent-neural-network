90cc99b10ed9b42595c17fa3dc5e0015593a308c
Daniel Chen <Maveri2k@yahoo.com>
2013-12-28 20:10:14 -0500

    scratchpad to test functionsa and how methods work

---
+++ Daniel/src/unused/Scratchpad.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package unused;
+
+/**
+ * @author dchen
+ * 
+ */
+public class Scratchpad {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// System.out.println("hello world");
+		int[] test = { 3, 4, 5, 6, 100, 345 };
+
+		System.out.println(test.length);
+	}
+
+}
