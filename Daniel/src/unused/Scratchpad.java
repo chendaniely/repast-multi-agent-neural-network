794f379c96c348f7bf6367b8e1e2a64f148e4249
Daniel Chen <Maveri2k@yahoo.com>
2013-12-29 00:49:23 -0500

    test snippits on arrays

--- Daniel/src/unused/Scratchpad.java
+++ Daniel/src/unused/Scratchpad.java
@@ -13,10 +13,16 @@ public class Scratchpad {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// System.out.println("hello world");
+		System.out.println("hello world");
+
 		int[] test = { 3, 4, 5, 6, 100, 345 };
 
 		System.out.println(test.length);
+
+		for (int i = 0; i < test.length - 1; i++) {
+			System.out.println(test[i]);
+		}
+		System.out.println(test[test.length - 1]);
 	}
 
 }
