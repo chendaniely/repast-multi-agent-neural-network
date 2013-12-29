91f8dd11a5aebbe41abcd61cf849ee61dd598637
Daniel Chen <Maveri2k@yahoo.com>
2013-12-28 20:18:15 -0500

    Added constant directory for agent variable weight output

--- Daniel/src/neuralNetworkABM/GlobalSpaceConstant.java
+++ Daniel/src/neuralNetworkABM/GlobalSpaceConstant.java
@@ -12,5 +12,6 @@ public final class GlobalSpaceConstant {
 	public final static int LENGTH_X = 10;
 	public final static int LENGTH_Y = 10;
 	public final static String agentVariableWeightLocation = "../files/agent variable weights.xls";
+	public final static String agentWeightValues = "./output/agentWeightCSV.csv";
 }
 
