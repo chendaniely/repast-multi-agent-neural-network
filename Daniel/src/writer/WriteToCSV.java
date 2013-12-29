79eb64545b577b3452ccb42c072afd3d01543f53
Daniel Chen <Maveri2k@yahoo.com>
2013-12-29 00:49:56 -0500

    Write array values to CSV (as row entities)
    adds new line to last array value

--- Daniel/src/writer/WriteToCSV.java
+++ Daniel/src/writer/WriteToCSV.java
@@ -3,18 +3,36 @@
  */
 package writer;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * @author dchen
- *
+ * 
  */
 public class WriteToCSV {
 
 	/**
+	 * @throws IOException
 	 * 
 	 */
-	public WriteToCSV(String fileDirectory, String[] arrayOfValues) {
+	public WriteToCSV(String fileDirectory, String[] arrayOfValues, boolean append)
+			throws IOException {
 		System.out.println("Writing to CSV file");
+
+		FileWriter writer = new FileWriter(fileDirectory, append);
 		
+		for (int i = 0; i < arrayOfValues.length - 1; i++) {
+			writer.append(arrayOfValues[i]);
+			writer.append(',');
+		}
+		// writes the last value and then puts in a newline
+		writer.append(arrayOfValues[arrayOfValues.length-1]);
+		writer.append('\n');
+		
+		writer.flush();
+		writer.close();
+
 	}
 
 }
