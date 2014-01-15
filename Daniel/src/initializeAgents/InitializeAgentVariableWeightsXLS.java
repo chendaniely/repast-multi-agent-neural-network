1432430b39ee0c0e7a0b32ec95ee31caf8650f5b
Daniel Chen <Maveri2k@yahoo.com>
2014-01-15 18:59:16 -0500

    Start separate XLS initialization class

---
+++ Daniel/src/initializeAgents/InitializeAgentVariableWeightsXLS.java
@@ -0,0 +1,20 @@
+package initializeAgents;
+
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.grid.Grid;
+
+public class InitializeAgentVariableWeightsXLS {
+
+  public InitializeAgentVariableWeightsXLS() {
+
+  }
+
+  // takes in the space, grid, agent number, and agent variable weight xls file
+  // generate agents and their variable weights according to input xls file
+  public void initialize(ContinuousSpace<Object> space, Grid<Object> grid, int agentNumber,
+      String variableWeightFilesName) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
