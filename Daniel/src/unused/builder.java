821f121d7054730e8abd640491cd63cef8618442
Daniel Chen <Maveri2k@yahoo.com>
2014-01-15 18:42:31 -0500

    commented out code from builder

---
+++ Daniel/src/unused/builder.java
@@ -0,0 +1,51 @@
+package unused;
+
+public class builder {
+  
+  //
+  // agentNetworkCSV = new FileWriter("./src/files/agentNetworkCSV.csv");
+  // agentNetworkCSV.append("Agent");
+  // agentNetworkCSV.append(',');
+  // agentNetworkCSV.append("--> Agent");
+  // agentNetworkCSV.append('\n');
+  
+  
+  
+
+  // InitializeAgentInfluences initializeAgentInfluences = new InitializeAgentInfluences();
+  // FileReader agentNetworkCSV1 = null;
+  // try {
+  // agentNetworkCSV1 = new FileReader("./src/files/agentNetworkCSV.csv");
+  // } catch (FileNotFoundException e2) {
+  // // TODO Auto-generated catch block
+  // e2.printStackTrace();
+  // }
+  // try {
+  // initializeAgentInfluences.assignAgentInfluences(context, agentNetworkCSV1);
+  // } catch (IOException e1) {
+  // // TODO Auto-generated catch block
+  // e1.printStackTrace();
+  // }
+
+
+  // System.out.println("hello");
+  // CSVReader reader = new CSVReader(new FileReader("./src/files/agentNetworkCSV.csv"));
+  // String [] nextLine;
+  // while ((nextLine = reader.readNext()) != null) {
+  // // nextLine[] is an array of values from the line
+  // System.out.println(nextLine[0] + "\t" + nextLine[1]);
+  // }
+  // reader.close();
+
+
+  // int i = 0;
+  // for (Object obj : context) {
+  // if (obj instanceof AgentABM) {
+  // System.out.println("agent number: " + ((AgentABM) obj).getAgentNumber());
+  // System.out.println("agent networkList: " + ((AgentABM) obj).getAgentNetworkList().get(i));
+  // System.out.println("agent influenced by: " + ((AgentABM) obj).getAgentInfluencedBy().get(i));
+  // }
+  // i ++;
+  // }
+
+}
