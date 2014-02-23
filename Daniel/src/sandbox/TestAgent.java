07019d4fce084b7f7ff16c0e8115ce08770f6020
Daniel Chen <Maveri2k@yahoo.com>
2014-02-23 14:41:55 -0500

    Test Agent, added int totalNumberOfProcessingUnits = -1

---
+++ Daniel/src/sandbox/TestAgent.java
@@ -0,0 +1,17 @@
+package sandbox;
+
+public class TestAgent {
+  
+  private int totalNumberOfProcessingUnits = -1;
+  
+  // GETTERS AND SETTERS
+
+  public int getTotalNumberOfProcessingUnits() {
+    return totalNumberOfProcessingUnits;
+  }
+
+  public void setTotalNumberOfProcessingUnits(int totalNumberOfProcessingUnits) {
+    this.totalNumberOfProcessingUnits = totalNumberOfProcessingUnits;
+  }
+
+}
