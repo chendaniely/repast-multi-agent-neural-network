08bd65ad4913c9ccc1cbc17aff7d107c6c9d75b4
Daniel Chen <Maveri2k@yahoo.com>
2014-02-24 02:43:53 -0500

    Class that will unit test functions

---
+++ Daniel/src/sandbox/UnitTest.java
@@ -0,0 +1,11 @@
+package sandbox;
+
+public class UnitTest {
+
+  public static void main(String[] args) {
+    TestAgent.setInitializationLog("./src/sandbox/testLog.log");
+    TestAgent.setProcessingUnitCSV("./src/sandbox/AgentProcessigUnitValues.csv");
+
+  }
+
+}
