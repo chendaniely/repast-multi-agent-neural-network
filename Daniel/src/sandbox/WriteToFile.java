994be96188bd23227373e3bca7256bfcc3b7855d
Daniel Chen <Maveri2k@yahoo.com>
2014-02-24 01:23:51 -0500

    Generic class that will write to a text file, mainly written for logging purposes

---
+++ Daniel/src/sandbox/WriteToFile.java
@@ -0,0 +1,46 @@
+package sandbox;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile {
+
+  /**
+   * @return
+   * @throws IOException
+   * 
+   */
+  private String fileDirectory = null;
+  private File file = null;
+  
+  // CONSTRUCTOR
+
+  public WriteToFile(String directory) throws IOException {
+    this.fileDirectory = directory;
+
+    file = new File(fileDirectory);
+
+    if (!file.exists()) {
+      file.createNewFile();
+    }
+    FileWriter fw = new FileWriter(fileDirectory, false);
+
+
+  }
+
+  public void write(String string) throws IOException {
+//    System.out.println("Writing to file");
+
+    FileWriter fw = new FileWriter(fileDirectory, true);
+    BufferedWriter bw = new BufferedWriter(fw);
+
+    bw.append(string + '\n');
+
+//    bw.flush();
+    bw.close();
+
+  }
+
+}
