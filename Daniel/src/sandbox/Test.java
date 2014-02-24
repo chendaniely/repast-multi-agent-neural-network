6db924a449429d7d9da186f1e16d0cbab53b451e
Daniel Chen <Maveri2k@yahoo.com>
2014-02-24 01:22:20 -0500

    linked csv to the class variable, instead of hard coding it in

--- Daniel/src/sandbox/Test.java
+++ Daniel/src/sandbox/Test.java
@@ -14,16 +14,30 @@ import au.com.bytecode.opencsv.CSVReader;
  */
 public class Test {
 
+  private String logDir = null;
+
+  void readProcessingUnitsCSV() {
+
+  }
+
   static void initializeAgentsFromCSV() throws IOException {
 
     // read in csv, and skips first line (headers)
     CSVReader reader =
-        new CSVReader(new FileReader("./src/sandbox/AgentProcessigUnitValues.csv"), ',', '\"', 1);
+        new CSVReader(new FileReader(TestAgent.getProcessingUnitCSV()), ',', '\"', 1);
 
     String[] nextLine;
+
     while ((nextLine = reader.readNext()) != null) {
-      // nextLine[] is an array of values from the line
-      System.out.println(nextLine[0] + nextLine[1] + "etc...");
+      // njareextLine[] is an array of values from the line
+
+      for (int i = 0; i < nextLine.length; i++) {
+        System.out.print(nextLine[i] + " ");
+      }
+      // System.out.println(nextLine[0] + " " + nextLine[1] + " etc...");
+      System.out.println(nextLine.length);
+
+
     }
   }
 
@@ -35,9 +49,26 @@ public class Test {
    * @param args
    */
   public static void main(String[] args) {
+    
+    // setting up the log file
+    WriteToFile log = null;
+    try {
+      log = new WriteToFile("./src/sandbox/testLog.log");
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
+
+    try {
+      log.write("hello");
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
+
     // print working directory
     System.out.println(System.getProperty("user.dir"));
 
+    // TestAgent initializeClassVariables = new TestAgent();
+    TestAgent.setProcessingUnitCSV("./src/sandbox/AgentProcessigUnitValues.csv");
 
     try {
       initializeAgentsFromCSV();
@@ -46,6 +77,8 @@ public class Test {
       e.printStackTrace();
     }
 
+
   }
+
 
 }
