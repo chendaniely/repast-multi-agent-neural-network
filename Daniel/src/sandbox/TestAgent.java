bfbb52e34aaaa872ba788aad3d1b0eed89e8a2c8
Daniel Chen <Maveri2k@yahoo.com>
2014-02-24 01:22:46 -0500

    added csv string variable + getters and setters

--- Daniel/src/sandbox/TestAgent.java
+++ Daniel/src/sandbox/TestAgent.java
@@ -2,9 +2,19 @@ package sandbox;
 
 public class TestAgent {
   
+  private static String processingUnitCSV = "";
+  
   private int totalNumberOfProcessingUnits = -1;
   
   // GETTERS AND SETTERS
+
+  public static String getProcessingUnitCSV() {
+    return processingUnitCSV;
+  }
+
+  public static void setProcessingUnitCSV(String processingUnitCSV) {
+    TestAgent.processingUnitCSV = processingUnitCSV;
+  }
 
   public int getTotalNumberOfProcessingUnits() {
     return totalNumberOfProcessingUnits;
