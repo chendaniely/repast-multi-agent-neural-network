276572d9430315740e1a73051babeb3579188f94
Daniel Chen <Maveri2k@yahoo.com>
2014-02-25 18:37:28 -0500

    Commented Variables, created array values for pos/neg t/t-1 processing units

--- Daniel/src/sandbox/TestAgent.java
+++ Daniel/src/sandbox/TestAgent.java
@@ -2,10 +2,39 @@ package sandbox;
 
 public class TestAgent {
 
+  // file where the processing unit csv is
   private static String processingUnitCSV = null;
+
+  // file where the agent log is
   private static String initializationLog = null;
+
+  // this is the number of processing units read from the CSV file, it includes the positive +
+  // negative processing units for each belief
   private static int totalNumberOfProcessingUnits = -1;
+
+  // this represents the combination of processing units in each valence bank CHOOSE 2 (since each
+  // processing unit will be connected to the other processing units in each valence bank)
   private static int totalNumberOfProcessingUnitWeights = -1;
+  
+  double[] positiveInputProcessingUnitsT0 = null;
+  double[] negativeInputProcessingUnitsT0 = null;
+  double[] positiveInputProcessingUnitsT_1 = null;
+  double[] negativeInputProcessingUnitsT_1 = null;
+  
+  double[] positiveOutputProcessingUnitsT0 = null;
+  double[] negativeOutputProcessingUnitsT0 = null;
+  double[] positiveOutputProcessingUnitsT_1 = null;
+  double[] negativeOutputProcessingUnitsT_1 = null;
+
+  /**
+   * This method will take in the totalNumberOfProcessingUnits and initialize an agent with 8 arrays
+   * of length = totalNumberOfProcessingUnits
+   * 
+   * @param processingUnitsPerBank
+   */
+  public void initializeFromCSV(int processingUnitsPerBank) {
+
+  }
 
 
 
