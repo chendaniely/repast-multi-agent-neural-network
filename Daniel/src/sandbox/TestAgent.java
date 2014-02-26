ec8650ead55baf1f70dfc601d00b4570fe0deaba
Daniel Chen <Maveri2k@yahoo.com>
2014-02-25 23:03:32 -0500

    populate T_1 array with csv numbers & directly calculate array length

--- Daniel/src/sandbox/TestAgent.java
+++ Daniel/src/sandbox/TestAgent.java
@@ -16,6 +16,9 @@ public class TestAgent {
   // processing unit will be connected to the other processing units in each valence bank)
   private static int totalNumberOfProcessingUnitWeights = -1;
 
+  // number of valence banks in agent
+  private static int numberOfValenceBanks = 2;
+
   double[] positiveInputProcessingUnitsT0 = null;
   double[] negativeInputProcessingUnitsT0 = null;
   double[] positiveInputProcessingUnitsT_1 = null;
@@ -26,23 +29,81 @@ public class TestAgent {
   double[] positiveOutputProcessingUnitsT_1 = null;
   double[] negativeOutputProcessingUnitsT_1 = null;
 
+  // Constructor(s)
+  public TestAgent() {
+    System.out.println("totalNumberOfProcessingUnits: " + totalNumberOfProcessingUnits);
+    /* @formatter:off */
+    System.out.println("Processing units per valence bank: " + totalNumberOfProcessingUnits / numberOfValenceBanks);
+    /* @formatter:on */
+  }
+
   /**
    * This method will take in the totalNumberOfProcessingUnits and initialize an agent with 8 arrays
    * of length = totalNumberOfProcessingUnits
    * 
    * @param processingUnitsPerBank
    */
-  public void initializeFromCSV(int processingUnitsPerBank) {
-    
-    this.positiveInputProcessingUnitsT0   = new double[processingUnitsPerBank]; 
-    this.negativeInputProcessingUnitsT0   = new double[processingUnitsPerBank]; 
-    this.positiveInputProcessingUnitsT_1  = new double[processingUnitsPerBank];
-    this.negativeInputProcessingUnitsT_1  = new double[processingUnitsPerBank];
+  public void initializeFromCSV() {
+    /* @formatter:off */
+    this.positiveInputProcessingUnitsT0   = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    this.negativeInputProcessingUnitsT0   = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
 
-    this.positiveOutputProcessingUnitsT0  = new double[processingUnitsPerBank];
-    this.negativeOutputProcessingUnitsT0  = new double[processingUnitsPerBank];
-    this.positiveOutputProcessingUnitsT_1 = new double[processingUnitsPerBank];
-    this.negativeOutputProcessingUnitsT_1 = new double[processingUnitsPerBank];
+    this.positiveInputProcessingUnitsT_1  = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    this.negativeInputProcessingUnitsT_1  = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+
+    this.positiveOutputProcessingUnitsT0  = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    this.negativeOutputProcessingUnitsT0  = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+
+    this.positiveOutputProcessingUnitsT_1 = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    this.negativeOutputProcessingUnitsT_1 = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    /* @formatter:on */
+  }
+
+  // Instance Methods
+  /**
+   * This will take a line and return a double array of processing unit values positive processing
+   * units for beliefs are odd indicies in the csv and begin with 1 even processing units for
+   * beliefs are even indicies in the csv and begin with 2, 0 is the agent number row and is skipped
+   * 
+   * @return
+   */
+  public void initializeValenceBanks(String[] nextLine) {
+    // /////////////////
+    System.out.print('\n');
+    for (String string : nextLine) {
+      System.out.print(string + ", ");
+    }
+    System.out.print('\n');
+    // //////////////////////
+
+    // skip the first index
+    int j = 0;
+    for (int i = 1; i < nextLine.length; i++) {
+      Double doubleOfStringValue = Double.valueOf(nextLine[i]);
+
+      // the index is odd (positive valence bank)
+      if ((i % 2) == 1) {
+        positiveInputProcessingUnitsT_1[j] = doubleOfStringValue;
+      }
+
+      // the index is even (negative valence bank)
+      if ((i % 2) == 0) {
+        negativeInputProcessingUnitsT_1[j] = doubleOfStringValue;
+        j++;
+      }
+
+    }
+    for (int i = 0; i < (totalNumberOfProcessingUnits / numberOfValenceBanks); i++) {
+      System.out.println("+ " + positiveInputProcessingUnitsT_1[i] + " | - "
+          + negativeInputProcessingUnitsT_1[i]);
+    }
+  }
+
+  public double calculateOutputFromInputs(double sameBank, double oppositeProcessingUnit,
+      double correspondingAgent) {
+    double output = 0;
+
+    return output;
 
   }
 
