87c2ec4d0da9b0ee323728438fe2e581cec96834
Daniel Chen <Maveri2k@yahoo.com>
2014-03-08 16:26:08 -0500

    Moved methods around to new classes

--- Daniel/src/sandbox/Calculation.java
+++ Daniel/src/sandbox/Calculation.java
@@ -1,5 +1,73 @@
 package sandbox;
 
 public class Calculation {
+  
+  /**
+   * this will take doubles from each of the different forms of input, and calculate/return output
+   * 
+   * @param sameBank
+   * @param oppositeProcessingUnit
+   * @param correspondingAgent
+   * @return
+   */
+  public double calculateOutputFromInputs(double sameBank, double oppositeProcessingUnit,
+      double correspondingAgent) {
+    double output = 0;
+
+    return output;
+
+  }
+  
+  /**
+   * takes 2 lists, first are the values of the same valence bank
+   * second is a list of weights between each valence bank
+   * this method is also hard coded (for the time being)
+   * @return
+   */
+  public double calculateValenceBankInput(double[] values, double[] weights) {
+    double input = .1;
+
+    for (int i = 0; i < totalNumberOfProcessingUnits / numberOfValenceBanks - 1; i++) {
+      // minus one is becuase we do not need to count the processing unit's weight to itself
+      System.out.println("index: " + i);
+      System.out.println("input: " + input);
+      System.out.println("value: " + values[i] + " weight: " + weights[i]);
+      input += (values[i] * weights[i]);
+      System.out.println(input);
+      
+    }
+    return input;
+  }
+  
+  public double calculateOppositeProcessingUnit(double input){
+    System.out.println("opposite processing unit: " + input);
+    input = input*-.2;
+    
+    System.out.println("final input from Opposite Processing unit: " + input);
+    return input;
+  }
+  
+  /**
+   * takes in an array of all the corresponding inputs,
+   * sums them, mult by 5
+   * @param input
+   * @return
+   */
+  public double calculateCorrespondingProcessingUnit(double[] input){
+    double output = 0;
+    double total = 0;
+    for (int i = 0; i < input.length; i++){
+      total += input[i];
+    }
+    output = total * .5;
+    return output;
+  }
+  
+  public double convertToLogit(double input){
+    input =  (1.0 / (1 + Math.exp(input)));
+    
+    return input;
+    
+  }
 
 }
--- Daniel/src/sandbox/Initialization.java
+++ Daniel/src/sandbox/Initialization.java
@@ -1,5 +1,96 @@
 package sandbox;
 
-public class Initialization {
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Initialization {
+  
+
+  /**
+   * This method will take in the totalNumberOfProcessingUnits and initialize an agent with 8 arrays
+   * of length = totalNumberOfProcessingUnits
+   * 
+   * @param processingUnitsPerBank
+   */
+  public void initializeValuesFromCSV() {
+    /* @formatter:off */
+    this.positiveInputProcessingUnitsT0   = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    this.negativeInputProcessingUnitsT0   = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+
+    this.positiveInputProcessingUnitsT_1  = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    this.negativeInputProcessingUnitsT_1  = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+
+    this.positiveOutputProcessingUnitsT0  = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    this.negativeOutputProcessingUnitsT0  = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+
+    this.positiveOutputProcessingUnitsT_1 = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    this.negativeOutputProcessingUnitsT_1 = new double[totalNumberOfProcessingUnits / numberOfValenceBanks];
+    /* @formatter:on */
+  }
+
+  /**
+   * this method is hardcoded
+   * @throws IOException 
+   */
+  public void initializeWeightsFromCSV(String csvWeightFile) throws IOException {
+    // int bankConnections = 10;
+//    positiveWeightProcessingUnit = new double[] {-1.0, -0.8, -0.6, -0.4,    // 0
+//                                                 -0.2, 0.3, 0.5,            // 1
+//                                                 0.7, 0.9,                  // 2
+//                                                 1.0};                      // 3
+    CSVReader reader = new CSVReader(new FileReader(csvWeightFile));
+    String [] nextLine;
+    int i = 0;
+    // find the line of interest that represents the current agent's weight values
+    while ((nextLine = reader.readNext()) != null) {
+      System.out.println(nextLine[0] + nextLine[1] + "etc...");
+    }
+    
+  }
+  // TODO generate method that will dynamically create what the value and weight arrays will be
+  /**
+   * takes in an int (that corresponds to the array index)
+   * takes the list of weights, and generates a new list with the corresponding weights
+   */
+  /**
+   * This will take a line and return a double array of processing unit values positive processing
+   * units for beliefs are odd indicies in the csv and begin with 1 even processing units for
+   * beliefs are even indicies in the csv and begin with 2, 0 is the agent number row and is skipped
+   * 
+   * @return
+   */
+  public void initializeValenceBanks(String[] nextLine) {
+
+    /* @formatter:off */
+    System.out.print("\nFrom TestAgent Class: ");   //
+    for (String string : nextLine) {                // Print statements
+      System.out.print(string + ", ");              //
+    }                                               //
+    System.out.print('\n');                         //
+    /* @formatter:on */
+
+    int j = 0;
+    // skip the first index
+    for (int i = 1; i < nextLine.length; i++) {
+      Double doubleOfStringValue = Double.valueOf(nextLine[i]);
+
+      // the index is odd (positive valence bank)
+      if ((i % 2) == 1) {
+        positiveInputProcessingUnitsT_1[j] = doubleOfStringValue;
+      }
+
+      // the index is even (negative valence bank)
+      if ((i % 2) == 0) {
+        negativeInputProcessingUnitsT_1[j] = doubleOfStringValue;
+        j++;
+      }
+
+    }
+    /* @formatter:off */
+    for (int i = 0; i < (totalNumberOfProcessingUnits / numberOfValenceBanks); i++) {   // Print
+      System.out.println("+ " + positiveInputProcessingUnitsT_1[i] + " | - "            //
+          + negativeInputProcessingUnitsT_1[i]);                                        //
+    }                                                                                   //
+    /* @formatter:on */
+  }
 }
