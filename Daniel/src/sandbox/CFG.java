3ac070f632668b71aa0a01e457659df0098e83c5
Daniel Chen <Maveri2k@yahoo.com>
2014-03-10 23:44:22 -0400

    global configuration class

---
+++ Daniel/src/sandbox/CFG.java
@@ -0,0 +1,13 @@
+package sandbox;
+
+public class CFG {
+
+  final static double EPSILON = 0.00001;
+
+  final static int LENGTH_X = 10;
+  final static int LENGTH_Y = 10;
+
+  static final String PROCESSING_UNIT_VALUES_CSV = null;
+  static final String PROCESSING_UNIT_WEIGHTS_CSV = null;
+
+}
