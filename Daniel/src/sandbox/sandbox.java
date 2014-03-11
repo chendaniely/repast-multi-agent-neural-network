4d2003b8193b7120d3667fb1f25140df858171a4
Daniel Chen <Maveri2k@yahoo.com>
2014-03-10 23:43:31 -0400

    A place to test out really random snippits of code

---
+++ Daniel/src/sandbox/sandbox.java
@@ -0,0 +1,16 @@
+package sandbox;
+
+public class sandbox {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("hello");
+    
+    double[] doublearray = null;
+    double[] testarray = new double[]{3.4, 4.5, 6.6};
+
+    doublearray = testarray;
+    
+  }
+
+}
