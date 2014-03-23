d68376e595593ef83d6bbcabf5a876e0d37226c6
Daniel Chen <Maveri2k@yahoo.com>
2014-03-22 21:00:10 -0400

    started generateValenceBankWeights method, nothing in it

--- Daniel/src/sandbox/Generation.java
+++ Daniel/src/sandbox/Generation.java
@@ -2,4 +2,13 @@ package sandbox;
 
 public class Generation {
 
+  public Generation() {
+
+  }
+
+  public double[] generateValenceBankWeights(int puIndex, int numberOfPUInEachVB, double[] VBWeights) {
+    
+    return VBWeights;
+
+  }
 }
