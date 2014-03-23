016989a30d6602c71c262eeee4ac1d2447e46658
Daniel Chen <Maveri2k@yahoo.com>
2014-03-22 20:48:17 -0400

    added value csv dir, and fixed weights

--- Daniel/src/sandbox/CFG.java
+++ Daniel/src/sandbox/CFG.java
@@ -7,7 +7,10 @@ public class CFG {
   final static int LENGTH_X = 10;
   final static int LENGTH_Y = 10;
 
-  static final String PROCESSING_UNIT_VALUES_CSV = null;
+  static final String PROCESSING_UNIT_VALUES_CSV = "./src/sandbox/AgentProcessigUnitValues.csv";
   static final String PROCESSING_UNIT_WEIGHTS_CSV = null;
+
+  static final double OPPOSITE_PROCESSING_UNIT_WEIGHT = -.2;
+  static final double CORRESPONDING_PROCESSING_UNIT_WEIGHT = .5;
 
 }
