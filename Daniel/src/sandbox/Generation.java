9c46647e7494343d48c30382e8b2204b5e9f0e9b
Daniel Chen <Maveri2k@yahoo.com>
2014-03-30 02:28:49 -0400

    Implemented generateValenceBankWeightArrays

--- Daniel/src/sandbox/Generation.java
+++ Daniel/src/sandbox/Generation.java
@@ -6,14 +6,40 @@ public class Generation {
 
   }
 
-  public double[] generatePosNegValuesFromAllValues(double[] listOfAllValues) {
-
-    return null;
-  }
-
-  public double[] generateValenceBankWeights(int puIndex, int numberOfPUInEachVB, double[] VBWeights) {
-
-    return VBWeights;
-
+  /**
+   * @param puIndex index of the processing unit that needs a weight array generated.
+   * @param numberOfPUInEachVB number of processing units in each valence bank
+   * @param VBWeights is the array that contains all the weights (from the csv file)
+   * @return arrayOfWeights returns the weights that are the inputs for the processing unit
+   */
+  public double[][] generateValenceBankWeightArrays(int puIndex, int numberOfPUInEachVB,
+      double[] vbWeights) {
+    double[][] arrayOfWeights = new double[numberOfPUInEachVB][numberOfPUInEachVB];
+    int vbWeighti = 0;
+    int nMinusIndex = 0;
+    int totalSkip = 0;
+    for (int processingUnit = 0; processingUnit < numberOfPUInEachVB; processingUnit++) {
+      nMinusIndex = numberOfPUInEachVB - processingUnit;
+      for (int i = 0; i < numberOfPUInEachVB; i++) {
+        if (i == processingUnit) {
+          arrayOfWeights[processingUnit][i] = 0;
+          System.out.println("PU #" + processingUnit + "; Index #" + i + ": "
+              + arrayOfWeights[processingUnit][i]);
+        } else {
+          if (i < processingUnit) {
+            arrayOfWeights[processingUnit][i] = arrayOfWeights[i][processingUnit];
+            System.out.println("PU #" + processingUnit + "; Index #" + i + ": "
+                + arrayOfWeights[processingUnit][i]);
+          } else {
+            arrayOfWeights[processingUnit][i] = vbWeighti;
+            System.out.println("PU #" + processingUnit + "; Index #" + i + ": "
+                + arrayOfWeights[processingUnit][i]);
+            vbWeighti++;
+          }
+        }
+      }
+      totalSkip = totalSkip + nMinusIndex;
+    }
+    return arrayOfWeights;
   }
 }
