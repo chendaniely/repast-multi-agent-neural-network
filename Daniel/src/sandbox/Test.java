29e2318d04d6e0f4bb80a7ba1c24c0718e8957a1
Daniel Chen <Maveri2k@yahoo.com>
2014-04-01 13:07:04 -0400

    renamed variables, added method to initialize activation and weight from csv

--- Daniel/src/sandbox/Test.java
+++ Daniel/src/sandbox/Test.java
@@ -18,6 +18,13 @@ import au.com.bytecode.opencsv.CSVReader;
  * 
  */
 public class Test {
+  public static double[] convertStringArrayToDouble(String[] stringArray) {
+    double[] doubleArray = new double[stringArray.length];
+    for (int i = 0; i < stringArray.length; i++) {
+      doubleArray[i] = Double.parseDouble(stringArray[i]);
+    }
+    return doubleArray;
+  }
 
   /**
    * Method will initialize x number of agents, depending on how many non empty rows are in the csv
@@ -30,13 +37,17 @@ public class Test {
     ArrayList<TestAgent> agents = new ArrayList<TestAgent>();
 
     // read in csv, commas as delimiters between double-quoted strings, skips first line (headers),
-    CSVReader reader = new CSVReader(new FileReader(CFG.PROCESSING_UNIT_VALUES_CSV), ',', '\"', 1);
+    CSVReader value = new CSVReader(new FileReader(CFG.PROCESSING_UNIT_VALUES_CSV), ',', '\"', 1);
+
+    // read in csv, commas as delimiters between double-quoted strings, skips first line (headers),
+    CSVReader weight = new CSVReader(new FileReader(CFG.PROCESSING_UNIT_WEIGHTS_CSV), ',', '\"', 1);
 
     // nextLine[] is an array of values from the line
-    String[] nextLine;
+    String[] nextValueLine;
+    String[] nextWeightLine;
 
     int agendID = 0;
-    while ((nextLine = reader.readNext()) != null) {
+    while ((nextValueLine = value.readNext()) != null) {
       agendID++;
 
       TestAgent testAgent = new TestAgent();
@@ -52,29 +63,39 @@ public class Test {
       }
 
       // make sure the agent # assigned == agent # from csv
-      int agentNumber = Integer.parseInt(nextLine[0]);
-      System.out.println(testAgent.getAgentID() + " " + Integer.parseInt(nextLine[0]));
+      int agentNumber = Integer.parseInt(nextValueLine[0]);
+      System.out.println(testAgent.getAgentID() + " " + Integer.parseInt(nextValueLine[0]));
 
       // why is this assert statement not doing its job?!?!?
       System.out.println(testAgent.getAgentID() == agentNumber);
       assert (testAgent.getAgentID() == agentNumber);
 
       // assign the value array into each agent
-      String[] values = Arrays.copyOfRange(nextLine, 1, nextLine.length);
+      String[] values = Arrays.copyOfRange(nextValueLine, 1, nextValueLine.length);
 
       double[] valuesD = new double[values.length];
       for (int i = 0; i < valuesD.length; i++) {
         valuesD[i] = Double.parseDouble(values[i]);
       }
 
+      System.out.println("values read");
       System.out.println(Arrays.toString(valuesD));
 
       Initialization initializeAgents = new Initialization();
       testAgent.setProcessingUnitActivationValues(initializeAgents.initializeVBActivation(valuesD));
 
+      while ((nextWeightLine = weight.readNext()) != null) {
+        if (Double.parseDouble(nextWeightLine[0]) == agendID) {
+          System.out.println("hello");
+          System.out.println(Arrays.deepToString(initializeAgents.initializeVBWeights(agendID,
+              convertStringArrayToDouble(nextWeightLine))));
+        }
+      }
+
       agents.add(testAgent);
     }
-    reader.close();
+    value.close();
+    weight.close();
 
     return agents;
   }
