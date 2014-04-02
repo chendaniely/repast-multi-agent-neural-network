d3ac59878b81fd499b833832fcbb0974b2ddce65
Daniel Chen <Maveri2k@yahoo.com>
2014-04-02 18:22:30 -0400

    added edgelist file, changed to actual random data

--- Daniel/src/sandbox/CFG.java
+++ Daniel/src/sandbox/CFG.java
@@ -9,13 +9,16 @@ public class CFG {
   
   // it's important that the agent numbering is sequential and the same in both files
 
-  static final String PROCESSING_UNIT_VALUES_CSV = "./src/sandbox/AgentProcessigUnitValues.csv";
-  static final String PROCESSING_UNIT_WEIGHTS_CSV = "./src/sandbox/AgentProcessigUnitWeights.csv";
+  //static final String PROCESSING_UNIT_VALUES_CSV = "./src/sandbox/AgentProcessigUnitValues.csv";
+  //static final String PROCESSING_UNIT_WEIGHTS_CSV = "./src/sandbox/AgentProcessigUnitWeights.csv";
   
-  //static final String PROCESSING_UNIT_VALUES_CSV = "./src/sandbox/agentActivationValues.csv";
-  //static final String PROCESSING_UNIT_WEIGHTS_CSV = "./src/sandbox/agentWeights.csv";
+  static final String PROCESSING_UNIT_VALUES_CSV = "./src/sandbox/agentActivationValues.csv";
+  static final String PROCESSING_UNIT_WEIGHTS_CSV = "./src/sandbox/agentWeights.csv";
+  static final String EDGE_LIST_CSV = "./src/sandbox/agentEdgeList.csv";
 
   static final double OPPOSITE_PROCESSING_UNIT_WEIGHT = -.2;
   static final double CORRESPONDING_PROCESSING_UNIT_WEIGHT = .5;
+  
+  static final String DEL = ", ";
 
 }
