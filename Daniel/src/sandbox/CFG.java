e817bf75aef03d02735531038fe970902a6d062f
Daniel Chen <Maveri2k@yahoo.com>
2014-04-02 01:53:55 -0400

    comment: it's important that the agent numbering is sequential and the same in both files

--- Daniel/src/sandbox/CFG.java
+++ Daniel/src/sandbox/CFG.java
@@ -6,6 +6,8 @@ public class CFG {
 
   final static int LENGTH_X = 10;
   final static int LENGTH_Y = 10;
+  
+  // it's important that the agent numbering is sequential and the same in both files
 
   static final String PROCESSING_UNIT_VALUES_CSV = "./src/sandbox/AgentProcessigUnitValues.csv";
   static final String PROCESSING_UNIT_WEIGHTS_CSV = "./src/sandbox/AgentProcessigUnitWeights.csv";
