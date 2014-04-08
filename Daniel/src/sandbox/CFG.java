50ffb0007931d3ca2958cd623384378a9c8a9526
Daniel Chen <Maveri2k@yahoo.com>
2014-04-07 20:26:28 -0400

    put parenthesis around -.2

--- Daniel/src/sandbox/CFG.java
+++ Daniel/src/sandbox/CFG.java
@@ -9,14 +9,16 @@ public class CFG {
   
   // it's important that the agent numbering is sequential and the same in both files
 
+  // simple numbers used for testing
   //static final String PROCESSING_UNIT_VALUES_CSV = "./src/sandbox/AgentProcessigUnitValues.csv";
   //static final String PROCESSING_UNIT_WEIGHTS_CSV = "./src/sandbox/AgentProcessigUnitWeights.csv";
   
+  // randomly generated numbers used to model
   static final String PROCESSING_UNIT_VALUES_CSV = "./src/sandbox/agentActivationValues.csv";
   static final String PROCESSING_UNIT_WEIGHTS_CSV = "./src/sandbox/agentWeights.csv";
   static final String EDGE_LIST_CSV = "./src/sandbox/agentEdgeList.csv";
 
-  static final double OPPOSITE_PROCESSING_UNIT_WEIGHT = -.2;
+  static final double OPPOSITE_PROCESSING_UNIT_WEIGHT = (-.2);
   static final double CORRESPONDING_PROCESSING_UNIT_WEIGHT = .5;
   
   static final String DEL = ", ";
