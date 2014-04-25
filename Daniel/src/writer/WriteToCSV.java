5590fd6abf80bee71b53de287692d0e80a9d5475
Daniel Chen <Maveri2k@yahoo.com>
2014-04-24 20:00:57 -0400

    added method to write writeAgentTimeTickToFile; formatting

--- Daniel/src/writer/WriteToCSV.java
+++ Daniel/src/writer/WriteToCSV.java
@@ -3,8 +3,13 @@
  */
 package writer;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+
+import sandbox.CFG;
+import au.com.bytecode.opencsv.CSVWriter;
 
 /**
  * @author dchen
@@ -12,27 +17,64 @@ import java.io.IOException;
  */
 public class WriteToCSV {
 
-	/**
-	 * @throws IOException
-	 * 
-	 */
-	public WriteToCSV(String fileDirectory, String[] arrayOfValues, boolean append)
-			throws IOException {
-		System.out.println("Writing to CSV file");
+  public WriteToCSV() {}
 
-		FileWriter writer = new FileWriter(fileDirectory, append);
-		
-		for (int i = 0; i < arrayOfValues.length - 1; i++) {
-			writer.append(arrayOfValues[i]);
-			writer.append(',');
-		}
-		// writes the last value and then puts in a newline
-		writer.append(arrayOfValues[arrayOfValues.length-1]);
-		writer.append('\n');
-		
-		writer.flush();
-		writer.close();
 
-	}
+  /**
+   * @return
+   * @throws IOException
+   * 
+   */
+  public void writeArrayToCSV(String fileDirectory, String[] arrayOfValues, boolean append)
+      throws IOException {
+    System.out.println("Writing to CSV file");
+
+    FileWriter writer = new FileWriter(fileDirectory, append);
+
+    for (int i = 0; i < arrayOfValues.length - 1; i++) {
+      writer.append(arrayOfValues[i]);
+      writer.append(',');
+    }
+    // writes the last value and then puts in a newline
+    writer.append(arrayOfValues[arrayOfValues.length - 1]);
+    writer.append('\n');
+
+    writer.flush();
+    writer.close();
+
+  }
+
+  /**
+   * used in my sandbox package takes the current time tick, current agent, and array of PU
+   * activation values and writes to CSV file, in the array, 0 index are pos values, 1 index are
+   * negative values
+   * 
+   * @throws IOException
+   * 
+   */
+  public void writeAgentTimeTickToFile(int time, int agent,
+      double[][] processingUnitActivationValues) throws IOException {
+
+    File outputFile = new File(CFG.OUTPUT_FILE_CSV);
+    if (!outputFile.exists()) {
+      outputFile.createNewFile();
+    }
+    // FileOutputStream oFile = new FileOutputStream(outputFile, true);
+
+    CSVWriter writer = new CSVWriter(new FileWriter(CFG.OUTPUT_FILE_CSV, true), ',');
+    // // feed in array (or convert your data to an array)
+
+    String stringOfArray = Arrays.deepToString(processingUnitActivationValues);
+    String stringOfEntries =
+        Integer.toString(time) + "," + Integer.toString(agent) + "," + stringOfArray;
+
+    String[] entries = stringOfEntries.replace("[", "").replace("]", "").split(",");
+
+    System.out.println("writting to csv: " + Arrays.toString(entries));
+
+    writer.writeNext(entries);
+    writer.close();
+  }
+
 
 }
