337f4c78b42e08a2f7894c0228522539dff9ef78
Daniel Chen <Maveri2k@yahoo.com>
2014-04-30 15:18:10 -0400

    deleted commented lines
    the lines used to keep track of the sanity cases,
    fixed by renaming the input sanity cases

--- Daniel/src/sandbox/CFG.java
+++ Daniel/src/sandbox/CFG.java
@@ -1,7 +1,7 @@
 package sandbox;
 
 public class CFG {
-  static final int           NUMBER_OF_TIME_TICKS                 = 10000;
+  static final int           NUMBER_OF_TIME_TICKS                 = 5;
 
   static final double        EPSILON                              = 0.00001;
 
@@ -10,40 +10,29 @@ public class CFG {
 
   // output file
 
-  public static final String OUTPUT_FILE_CSV                      = "./src/sandbox/output/output.csv";
+  public static final String OUTPUT_FILE_CSV                      = "./src/sandbox/output/sanity7-nodecay.csv";
   static final String        DEL                                  = ",";
   static final String        NEW_LINE                             = "\n";
 
   // it's important that the agent numbering is sequential and the same in both files
-
-  // simple numbers used for testing
-  // static final String PROCESSING_UNIT_VALUES_CSV =
-  // "./src/sandbox/input/AgentProcessigUnitValues.csv";
-  // static final String PROCESSING_UNIT_WEIGHTS_CSV =
-  // "./src/sandbox/input/AgentProcessigUnitWeights.csv";
-  // static final String EDGE_LIST_CSV = "./src/sandbox/input/testAgentEdgeList.csv";
-
-  // randomly generated numbers used to model
-  // static final String PROCESSING_UNIT_VALUES_CSV =
-  // "./src/sandbox/input/agentActivationValues.csv";
-  // static final String PROCESSING_UNIT_WEIGHTS_CSV = "./src/sandbox/input/agentWeights.csv";
-  // static final String EDGE_LIST_CSV = "./src/sandbox/input/agentEdgeList.csv";
-
-  // randomly generated numbers used to model, single seed
-
-  // singleSeedPosOnlyOnesActivationValues
-  // singleSeedPosOnlyActivationValues
-  // singleSeedActivationValues
-  static final String        PROCESSING_UNIT_VALUES_CSV           = "./src/sandbox/input/singleSeedActivationValues.csv";
-  static final String        PROCESSING_UNIT_WEIGHTS_CSV          = "./src/sandbox/input/agentWeights.csv";
-  static final String        EDGE_LIST_CSV                        = "./src/sandbox/input/agentEdgeList.csv";
-
+  static final String        PROCESSING_UNIT_VALUES_CSV           
+    = "./src/sandbox/input/sanity/sanity-3-4,6-singleSeedActivationValues.csv";
+  
+  static final String        PROCESSING_UNIT_WEIGHTS_CSV          
+    = "./src/sandbox/input/sanity/sanity-1-6-agentWeights.csv";
+  
+  static final String        EDGE_LIST_CSV                       
+    = "./src/sandbox/input/sanity/sanity-6-agentEdgeList-10-multiple.csv";
+  
+  static final int           EDGE_LIST_CSV_READ_FROM_LINE         = 1;
+  
+  static final double        LOGIT_TUNE                           = 1;
+  
   // calculation weights
-  static final double        OPPOSITE_PROCESSING_UNIT_WEIGHT      = (-.02);
-  static final double        CORRESPONDING_PROCESSING_UNIT_WEIGHT = .5;
+  static final double        OPPOSITE_PROCESSING_UNIT_WEIGHT      = (-0.7);
+  static final double        CORRESPONDING_PROCESSING_UNIT_WEIGHT = 0.5;
   static final double        CARRY_OVER                           = 0.2;
   static final double        BIAS                                 = (0); // leave as 0
-  static final double        DECAY                                = (-.5);
-
+  static final double        DECAY                                = (0);
 
 }
