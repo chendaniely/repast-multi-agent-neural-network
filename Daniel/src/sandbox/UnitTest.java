829e644057b9e22323b24f99f818b66e0240a578
Daniel Chen <Maveri2k@yahoo.com>
2014-04-30 15:20:47 -0400

    implemented double almostEqual, assert calc new output using carryover

--- Daniel/src/sandbox/UnitTest.java
+++ Daniel/src/sandbox/UnitTest.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class UnitTest {
+  
+  public static boolean almostEqual(double a, double b){
+    return Math.abs(a-b)< CFG.EPSILON;
+}
+  
 
   public static void testCalculationClass() {
     // Unit test the Calculation class
@@ -50,22 +55,45 @@ public class UnitTest {
     assert (calculationUnitTest.calculateValenceBankInput(values2, weights2) - (-1.48) < CFG.EPSILON);
 
     // testing calculateOutputFromInputs()
-    assert (calculationUnitTest.calculateOutputFromInputs(-.5, 0, .5) == .5);
-    assert ((calculationUnitTest.calculateOutputFromInputs(-.5, 1, .5) - 0.73105857863) < CFG.EPSILON);
-    assert ((calculationUnitTest.calculateOutputFromInputs(0, .5, .5) - 0.73105857863) < CFG.EPSILON);
-    assert ((calculationUnitTest.calculateOutputFromInputs(0, 0, 1) - 0.73105857863) < CFG.EPSILON);
-    assert ((calculationUnitTest.calculateOutputFromInputs(-1.48, -0.04, 0.1) - 0.19466158359) < CFG.EPSILON);
+//    assert (calculationUnitTest.calculateOutputFromInputs(-.5, 0, .5, 0) == 0);
+    assert (almostEqual(calculationUnitTest.calculateOutputFromInputs(-.5, 0, .5, 0), 0));
+    
+    assert ((calculationUnitTest.calculateOutputFromInputs(-.5, 1, .5, 0) - 0.23105857863) < CFG.EPSILON);
+    assert (almostEqual(calculationUnitTest.calculateOutputFromInputs(-.5, 1, .5, 0), 0.23105857863));
+    
+//    assert ((calculationUnitTest.calculateOutputFromInputs(0, .5, .5, 0) - 0.73105857863) < CFG.EPSILON);
+    assert (almostEqual(calculationUnitTest.calculateOutputFromInputs(0, .5, .5, 0), 0.23105857863));
+    
+//    assert ((calculationUnitTest.calculateOutputFromInputs(0, 0, 1, 0) - 0.73105857863) < CFG.EPSILON);
+    assert (almostEqual(calculationUnitTest.calculateOutputFromInputs(0, 0, 1, 0), 0.23105857863));
+    
+//    assert ((calculationUnitTest.calculateOutputFromInputs(-1.48, -0.04, 0.1, 0) - 0.19466158359) < CFG.EPSILON);
+    assert (almostEqual(calculationUnitTest.calculateOutputFromInputs(-1.48, -0.04, 0.1, 0), -0.30533841641));
 
     // testing processing unit 1 positive bank new value
     double[] valuesPU1p = {0.1, 0.3, 0.5, 0.7, 0.9};
     double[] weightsPU1p = {0, -1, -0.8, -0.6, -0.4};
     double oppositePU1p = 0.2;
     double[] correspondingPU1p = {0.2};
-    assert (calculationUnitTest.calculateOutputFromInputs(
+    assert (almostEqual(calculationUnitTest.calculateOutputFromInputs(
         calculationUnitTest.calculateValenceBankInput(valuesPU1p, weightsPU1p),
         calculationUnitTest.calculateOppositeProcessingUnit(oppositePU1p),
-        calculationUnitTest.calculateCorrespondingProcessingUnit(correspondingPU1p)) - 0.19466158359) < CFG.EPSILON;
-  }
+        calculationUnitTest.calculateCorrespondingProcessingUnit(correspondingPU1p),
+        0), -0.30533841641));
+  
+  // testing processing unit 1 positive bank new value for sanity 6
+  double[] valuesPU1p1 = {0.1, 0.3, 0.5, 0.7, 0.9};
+  double[] weightsPU1p1 = {0, -1, -0.8, -0.6, -0.4};
+  double oppositePU1p1 = 0.2;
+  double[] correspondingPU1p1 = {0.2};
+  assert (almostEqual(calculationUnitTest.calculateOutputFromInputs(
+      calculationUnitTest.calculateValenceBankInput(valuesPU1p1, weightsPU1p1),
+      calculationUnitTest.calculateOppositeProcessingUnit(oppositePU1p1),
+      calculationUnitTest.calculateCorrespondingProcessingUnit(correspondingPU1p1),
+      0), -0.30533841641));
+}
+  
+  
 
   public static void testInitializationClass() {
     // Unit test the Initialization class
