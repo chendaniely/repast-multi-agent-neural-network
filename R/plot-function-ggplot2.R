3a3399a3b7e7ee72221c175e5f83002ee005f891
Daniel Chen <Maveri2k@yahoo.com>
2014-04-30 23:42:53 -0400

    line graphs using ggplot2

---
+++ R/plot-function-ggplot2.R
@@ -0,0 +1,34 @@
+require(ggplot2)
+require(scales)
+data <- "/home/dchen/git/repast-neural-network-agent-based-model/R/data/sanity10-5.csv"
+numTimeTicks <- 10
+numAgents <- 10
+x <- read.csv(data, header=FALSE, stringsAsFactors=FALSE)
+
+# Rename dataframe column names
+names(x) <- c("time", "agent",
+              "p1", "p2", "p3", "p4", "p5",
+              "n1", "n2", "n3", "n4", "n5")
+
+# Convert string factors into numerics
+for (i in 3:12) {
+  x[ ,i] <- as.numeric(x[ ,i])
+}
+
+x["pos.avg"] <- apply(x[, c(3,4,5,6,7)], 1, mean)
+x["neg.avg"] <- apply(x[, c(8,9,10,11,12)], 1, mean)
+
+rm(g)
+limit = "no"
+g <- ggplot(x, aes(x = time))
+g <- g + geom_line(aes(y = pos.avg, color = factor(agent), group = agent))
+g <- g + geom_line(aes(y = neg.avg, color = factor(agent), group = agent), linetype="dashed")
+g <- g + scale_color_discrete(name = "Agent")
+#g <- g + scale_y_continuous(labels = comma)
+g <- g + scale_x_continuous(breaks=c(0:10))
+if (limit == "yes") {
+  g <- g + scale_x_continuous(limits=c(0, 5))
+}
+g <- g + labs(title = "Average Valence Bank Activation Values Over Time", x = "Time Tick", y = "Processing Unit Activation Value")
+g
+
